f89c84ba24562c95963050406d10645eebce589b
Eduardo Castillo <lincidro@gmail.com>
2021-02-14 02:01:41 -0500

    feat: se agregan las imagenes en bd y frontend

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -6,27 +6,22 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
-
   private baseUrl = 'http://localhost:8080/api/products';
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
   getProductList(): Observable<Product[]> {
-    return this.httpClient.get<GetResponse>(this.baseUrl)
-      .pipe(
-        map(
-          response => response._embedded.products
-        )
-      );
+    return this.httpClient
+      .get<GetResponse>(this.baseUrl)
+      .pipe(map((response) => response._embedded.products));
   }
-
 }
 
 interface GetResponse {
   _embedded: {
     products: Product[];
-  }
+  };
 }
